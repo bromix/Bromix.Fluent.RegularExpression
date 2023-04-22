cfd1907d27718ec179d2cb0c2cacf3f6bff9032d
Matthias Bromisch <matthias.bromisch@gmail.com>
2023-04-22 23:31:57 +0200

    add initial code

---
+++ Bromix.Fluent.RegularExpression.Tests/PatternTests.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+
+namespace Bromix.Fluent.RegularExpression.Tests;
+
+public sealed class PatternTests
+{
+    [Theory]
+    [InlineData("(", @"\(")]
+    [InlineData(")", @"\)")]
+    [InlineData("[", @"\[")]
+    [InlineData("]", @"\]")]
+    [InlineData("+", @"\+")]
+    [InlineData("Hello", @"Hello")]
+    public void Pattern_With_Literal(string input, string expected)
+    {
+        Pattern.With().Literal(input).ToString().Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("(", @"\(?")]
+    [InlineData(")", @"\)?")]
+    [InlineData("+", @"\+?")]
+    [InlineData("Hello", @"(Hello)?")]
+    public void Pattern_With_Literal_And_Quantifier_Optional(string input, string expected)
+    {
+        Pattern.With().Literal(Quantifier.Optional, input).ToString().Should().Be(expected);
+    }
+
+    [Fact]
+    public void Pattern_With_StartOfLine()
+    {
+        Pattern.With().StartOfLine().ToString().Should().Be("^");
+    }
+
+    [Fact]
+    public void Pattern_With_EndOfLine()
+    {
+        Pattern.With().EndOfLine().ToString().Should().Be("$");
+    }
+
+    [Fact]
+    public void Pattern_With_Digit()
+    {
+        Pattern.With().Digit().ToString().Should().Be(@"\d");
+    }
+
+    [Fact]
+    public void Pattern_With_Digit_And_Quantifier()
+    {
+        Pattern.With().Digit(Quantifier.Exactly(4)).ToString().Should().Be(@"\d{4}");
+    }
+    
+    [Fact]
+    public void Pattern_With_Word()
+    {
+        Pattern.With().Word().ToString().Should().Be(@"\w");
+    }
+
+    [Fact]
+    public void Pattern_With_Word_And_Quantifier()
+    {
+        Pattern.With().Word(Quantifier.Exactly(4)).ToString().Should().Be(@"\w{4}");
+    }
+
+    [Fact]
+    public void OneOf()
+    {
+        var group = Pattern.With()
+            .OneOf(
+                Quantifier.Optional,
+                p => p.CharacterClass(Quantifier.AtMost(10), cc => cc.Range('0', '9')),
+                p => p.CharacterClass(Quantifier.AtMost(10), cc => cc.Range('a', 'z')),
+                p => p.CharacterClass(Quantifier.AtMost(10), cc => cc.Range('A', 'Z')))
+            .ToString();
+    }
+
+    [Fact]
+    public void NamedGroup()
+    {
+        var group = Pattern.With()
+            .Group("Text", g => g
+                .CharacterClass(cc => cc
+                    .Range('A', 'Z')))
+            .ToString();
+    }
+}
---
+++ Bromix.Fluent.RegularExpression/Pattern.cs
@@ -0,0 +1,263 @@
+﻿using System.Text;
+
+namespace Bromix.Fluent.RegularExpression;
+
+public sealed class Pattern
+{
+    public static Pattern With() => new();
+
+    private Pattern()
+    {
+    }
+
+    /// <summary>
+    /// Returns a <see cref="Pattern"/> that matches the first, second or and any additional characters provided. 
+    /// </summary>
+    /// <param name="first">The first possible character to match.</param>
+    /// <param name="second">The second possible character to match.</param>
+    /// <param name="additional">Any additional character to match.</param>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern OneOf(char first, char second, params char[] additional)
+    {
+        return OneOf(Quantifier.None, first, second, additional);
+    }
+
+    /// <summary>
+    /// Returns a <see cref="Pattern"/> that matches the first, second or and any additional characters provided.
+    /// </summary>
+    /// <param name="quantifier">The <see cref="Quantifier"/> to use for the character class (e.g. '*', '+', '?', '{3}', '{2,4}', etc.).</param>
+    /// <param name="first">The first possible character to match.</param>
+    /// <param name="second">The second possible character to match.</param>
+    /// <param name="additional">Any additional character to match.</param>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern OneOf(Quantifier quantifier, char first, char second, params char[] additional)
+    {
+        CharacterClass cc = new(quantifier);
+        cc.OneOf(first, second, additional);
+        _stringBuilder.Append(cc);
+        return this;
+    }
+
+    /// <summary>
+    /// Returns a <see cref="Pattern"/> that matches the character class specified by the given <paramref name="characterClass"/> delegate.
+    /// </summary>
+    /// <param name="characterClass"></param>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern CharacterClass(Action<CharacterClass> characterClass)
+    {
+        return CharacterClass(Quantifier.None, characterClass);
+    }
+
+    /// <summary>
+    /// Returns a <see cref="Pattern"/> that matches the character class specified by the given <paramref name="characterClass"/> delegate. 
+    /// </summary>
+    /// <param name="quantifier">The <see cref="Quantifier"/> to use for the character class (e.g. '*', '+', '?', '{3}', '{2,4}', etc.).</param>
+    /// <param name="characterClass"></param>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern CharacterClass(Quantifier quantifier, Action<CharacterClass> characterClass)
+    {
+        CharacterClass cc = new(quantifier);
+        characterClass.Invoke(cc);
+        _stringBuilder.Append(cc);
+        return this;
+    }
+
+    /// <summary>
+    /// Returns a <see cref="Pattern"/> that matches the group specified by the given <paramref name="group"/> delegate.
+    /// </summary>
+    /// <param name="group"></param>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern Group(Action<Pattern> group)
+    {
+        return Group(Quantifier.None, group);
+    }
+
+    /// <summary>
+    /// Returns a <see cref="Pattern"/> that matches the group specified by the given <paramref name="group"/> delegate.
+    /// </summary>
+    /// <param name="quantifier">The <see cref="Quantifier"/> to use for the group.</param>
+    /// <param name="group"></param>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern Group(Quantifier quantifier, Action<Pattern> group)
+    {
+        Pattern g = new();
+        group.Invoke(g);
+        _stringBuilder.Append($"({g}){quantifier}");
+        return this;
+    }
+
+    /// <summary>
+    /// Returns a <see cref="Pattern"/> that matches the named group specified by the given <paramref name="group"/> delegate.
+    /// </summary>
+    /// <param name="name">The name to assign to the capturing group.</param>
+    /// <param name="group"></param>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern Group(string name, Action<Pattern> group)
+    {
+        return Group(Quantifier.None, name, group);
+    }
+
+    /// <summary>
+    /// Returns a <see cref="Pattern"/> that matches the named group specified by the given <paramref name="group"/> delegate.
+    /// </summary>
+    /// <param name="quantifier">The <see cref="Quantifier"/> to use for the named group.</param>
+    /// <param name="name">The name to assign to the capturing group.</param>
+    /// <param name="group"></param>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern Group(Quantifier quantifier, string name, Action<Pattern> group)
+    {
+        Pattern g = new();
+        group.Invoke(g);
+        _stringBuilder.Append($"(?<{name}>{g}){quantifier}");
+        return this;
+    }
+
+    /// <summary>
+    /// Returns a <see cref="Pattern"/> that matches one of patterns specified by the given <paramref name="first"/>, <paramref name="second"/>, and <paramref name="additional"/> <see cref="Action{T}"/> delegates.
+    /// </summary>
+    /// <param name="first"></param>
+    /// <param name="second"></param>
+    /// <param name="additional"></param>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern OneOf(Action<Pattern> first, Action<Pattern> second, params Action<Pattern>[] additional)
+    {
+        return OneOf(Quantifier.None, first, second, additional);
+    }
+
+    /// <summary>
+    /// Returns a <see cref="Pattern"/> that matches one of patterns specified by the given <paramref name="first"/>, <paramref name="second"/>, and <paramref name="additional"/> <see cref="Action{T}"/> delegates. 
+    /// </summary>
+    /// <param name="quantifier">The <see cref="Quantifier"/> to use for the group.</param>
+    /// <param name="first"></param>
+    /// <param name="second"></param>
+    /// <param name="additional"></param>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern OneOf(Quantifier quantifier, Action<Pattern> first, Action<Pattern> second,
+        params Action<Pattern>[] additional)
+    {
+        var firstPattern = With();
+        first.Invoke(firstPattern);
+        var secondPattern = With();
+        second.Invoke(secondPattern);
+        _stringBuilder.AppendFormat("({0}|{1}", firstPattern, secondPattern);
+
+        foreach (var choice in additional)
+        {
+            var additionalPattern = With();
+            choice.Invoke(additionalPattern);
+            _stringBuilder.AppendFormat("|{0}", additionalPattern);
+        }
+
+        _stringBuilder
+            .Append(")")
+            .Append(quantifier);
+        return this;
+    }
+
+    /// <summary>
+    /// Appends a literal string to the current <see cref="Pattern"/> object.
+    /// Any characters in the string that need to be escaped in a regular expression pattern will be automatically escaped.
+    /// </summary>
+    /// <param name="literal">The literal string to append to the pattern.</param>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern Literal(string literal) => Literal(Quantifier.None, literal);
+
+    /// <summary>
+    /// Appends a literal string to the current <see cref="Pattern"/> object.
+    /// Any characters in the string that need to be escaped in a regular expression pattern will be automatically escaped.
+    /// </summary>
+    /// <param name="quantifier">The <see cref="Quantifier"/> to use for the literal.</param>
+    /// <param name="literal">The literal string to append to the pattern.</param>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern Literal(Quantifier quantifier, string literal)
+    {
+        var group = literal.Length > 1 && quantifier != Quantifier.None;
+        if (group)
+        {
+            _stringBuilder.Append("(");
+        }
+
+        foreach (var c in literal)
+        {
+            if (CharsToEscape.Contains(c))
+            {
+                _stringBuilder.Append($@"\{c}");
+            }
+            else
+            {
+                _stringBuilder.Append(c);
+            }
+        }
+
+        if (group)
+        {
+            _stringBuilder.Append(")");
+        }
+
+        _stringBuilder.Append(quantifier);
+
+        return this;
+    }
+
+    /// <summary>
+    /// Returns a <see cref="Pattern"/> that matches any digit character.
+    /// </summary>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern Digit() => Digit(Quantifier.None);
+
+    /// <summary>
+    /// Returns a <see cref="Pattern"/> that matches any digit character.
+    /// </summary>
+    /// <param name="quantifier">The <see cref="Quantifier"/> to use for the digit.</param>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern Digit(Quantifier quantifier)
+    {
+        _stringBuilder.Append($@"\d{quantifier}");
+        return this;
+    }
+
+    /// <summary>
+    /// Returns a <see cref="Pattern"/> that matches any word character.
+    /// </summary>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern Word() => Word(Quantifier.None);
+
+    /// <summary>
+    /// Returns a <see cref="Pattern"/> that matches any word character. 
+    /// </summary>
+    /// <param name="quantifier">The <see cref="Quantifier"/> to use for the word.</param>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern Word(Quantifier quantifier)
+    {
+        _stringBuilder.Append($@"\w{quantifier}");
+        return this;
+    }
+
+    /// <summary>
+    /// Appends the "^" anchor to the current <see cref="Pattern"/> object to match the start of a line.
+    /// </summary>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public Pattern StartOfLine()
+    {
+        _stringBuilder.Append("^");
+        return this;
+    }
+
+    /// <summary>
+    /// Appends the "$" anchor to the current <see cref="Pattern"/> object to match the end of a line.
+    /// </summary>
+    /// <returns>A <see cref="Pattern"/> object that represents the regular expression pattern.</returns>
+    public object EndOfLine()
+    {
+        _stringBuilder.Append("$");
+        return this;
+    }
+
+    public override string ToString()
+    {
+        return _stringBuilder.ToString();
+    }
+
+    private const string CharsToEscape = @"()[]+";
+    private readonly StringBuilder _stringBuilder = new();
+}
