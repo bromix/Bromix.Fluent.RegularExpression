831cd4762a7510f40a9991cb22aa48790e766658
Matthias Bromisch <matthias.bromisch@gmail.com>
2023-04-23 18:15:40 +0200

    fix: add missing XML Comments.

--- Bromix.Fluent.RegularExpression/Quantifier.cs
+++ Bromix.Fluent.RegularExpression/Quantifier.cs
@@ -1,14 +1,46 @@
 ﻿namespace Bromix.Fluent.RegularExpression;
 
+/// <summary>
+/// Represents a quantifier for a regular expression pattern.
+/// </summary>
 public sealed class Quantifier
 {
+    /// <summary>
+    /// Gets a <see cref="Quantifier"/> instance that represents no quantifier.
+    /// </summary>
     public static readonly Quantifier None = new(string.Empty);
+
+    /// <summary>
+    /// Gets a <see cref="Quantifier"/> instance that represents zero or more occurrences of the previous element.
+    /// </summary>
     public static readonly Quantifier ZeroOrMore = new("*");
+
+    /// <summary>
+    /// Gets a <see cref="Quantifier"/> instance that represents zero or more occurrences of the previous element, matched lazily.
+    /// </summary>
     public static readonly Quantifier ZeroOrMoreLazy = new("*?");
+
+    /// <summary>
+    /// Gets a <see cref="Quantifier"/> instance that represents one or more occurrences of the previous element.
+    /// </summary>
     public static readonly Quantifier OneOrMore = new("+");
+
+    /// <summary>
+    /// /// Gets a <see cref="Quantifier"/> instance that represents one or more occurrences of the previous element, matched lazily.
+    /// </summary>
     public static readonly Quantifier OneOrMoreLazy = new("+?");
+
+    /// <summary>
+    /// Gets a <see cref="Quantifier"/> instance that represents zero or one occurrences of the previous element.
+    /// </summary>
     public static readonly Quantifier Optional = new("?");
 
+    /// <summary>
+    /// Gets a <see cref="Quantifier"/> instance that represents exactly the specified number of occurrences of the previous element.
+    /// </summary>
+    /// <param name="count">The number of occurrences.</param>
+    /// <returns>A <see cref="Quantifier"/> instance that represents the specified number of occurrences.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="count"/> is less than or equal to 0.</exception>
     public static Quantifier Exactly(int count)
     {
         return count switch
@@ -19,6 +51,12 @@ public sealed class Quantifier
         };
     }
 
+    /// <summary>
+    /// Gets a <see cref="Quantifier"/> instance that represents at least the specified number of occurrences of the previous element.
+    /// </summary>
+    /// <param name="count">The minimum number of occurrences.</param>
+    /// <returns>A <see cref="Quantifier"/> instance that represents at least the specified number of occurrences.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="count"/> is less than 0.</exception>
     public static Quantifier AtLeast(int count)
     {
         return count switch
@@ -30,6 +68,12 @@ public sealed class Quantifier
         };
     }
 
+    /// <summary>
+    /// Gets a <see cref="Quantifier"/> instance that represents at most the specified number of occurrences of the previous element.
+    /// </summary>
+    /// <param name="count">The maximum number of occurrences.</param>
+    /// <returns>A <see cref="Quantifier"/> instance that represents at most the specified number of occurrences.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="count"/> is less than or equal to 0.</exception>
     public static Quantifier AtMost(int count)
     {
         return count switch
@@ -40,6 +84,13 @@ public sealed class Quantifier
         };
     }
 
+    /// <summary>
+    /// Gets a <see cref="Quantifier"/> instance that represents a specific range of occurrences of the preceding element.
+    /// </summary>
+    /// <param name="min">The minimum number of occurrences of the preceding element. Must be non-negative.</param>
+    /// <param name="max">The maximum number of occurrences of the preceding element. Must be non-negative and greater than or equal to <paramref name="min"/>.</param>
+    /// <returns>A <see cref="Quantifier"/> that matches a specific range of occurrences of the preceding element.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="min"/> or <paramref name="max"/> is negative, or when <paramref name="max"/> is less than <paramref name="min"/>.</exception>
     public static Quantifier Range(int min, int max)
     {
         if (min < 0)
@@ -70,6 +121,10 @@ public sealed class Quantifier
         };
     }
 
+    /// <summary>
+    /// Returns the quantifier for a regular expression pattern.
+    /// </summary>
+    /// <returns>The quantifier for a regular expression pattern.</returns>
     public override string ToString() => _value;
     private Quantifier(string value) => _value = value;
     private readonly string _value;
